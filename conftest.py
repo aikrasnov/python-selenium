dcd7ae9366e3e799d536f2bbbef445195c9a2328
akrasnov <aleksander.krasnoff@ya.ru>
2018-12-21 01:00:56 +0300

    try use travis

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from selenium import webdriver
 
@@ -11,10 +12,33 @@ AVAILABLE_BROWSERS = {
 def driver(request, param):
     browser_type = request.config.getoption("--browser") or param
 
+    username = None
+    access_key = None
+
+    try:
+        username = "aikrasnov" # os.environ["SAUCE_USERNAME"]
+        access_key = "e6968af3-55c0-455e-b712-f6ef086f5640" # os.environ["SAUCE_ACCESS_KEY"]
+    except KeyError:
+        pass
+
+    has_sauce_lab = username and access_key
+    caps = {}
+    command_executor = f"https://{username}:{access_key}@ondemand.saucelabs.com/wd/hub"
+
     if AVAILABLE_BROWSERS["chrome"] in browser_type:
-        browser = webdriver.Chrome()
+        if has_sauce_lab:
+            caps.update({"browserName": "chrome", "platform": "Windows 10", "version": "71.0"})
+            browser = webdriver.Remote(desired_capabilities=caps, command_executor=command_executor)
+        else:
+            browser = webdriver.Chrome(desired_capabilities=caps)
+
     elif AVAILABLE_BROWSERS["firefox"] in browser_type:
-        browser = webdriver.Firefox()
+        if has_sauce_lab:
+            caps.update({"browserName": "firefox", "platform": "Windows 10", "version": "64.0"})
+            browser = webdriver.Remote(desired_capabilities=caps, command_executor=command_executor)
+        else:
+            browser = webdriver.Firefox(desired_capabilities=caps)
+
     else:
         raise RuntimeError(f"Unknown browser ${browser_type}")
 
@@ -24,7 +48,7 @@ def driver(request, param):
 
 @pytest.fixture(scope="function")
 def param(request):
-    # we don't have request.param when not parametrize tests with pytest_generate_tests
+    # we don"t have request.param when not parametrize tests with pytest_generate_tests
     try:
         return request.param
     except AttributeError:
--- readme.md
+++ readme.md
@@ -1,6 +1,17 @@
-### How to run
+## How to run UI tests
+### Local
+0) Install firefox, chrome and put theirs drivers in PATH ([chromedriver](http://chromedriver.chromium.org/), [geckodriver](https://github.com/mozilla/geckodriver/releases))
 1) Clone this repo
 2) Install pipenv ([instruction](https://github.com/pypa/pipenv#installation))
 3) pipenv install --dev
 4) pipenv shell
 5) pytest
+
+### Change count parallel tests
+`pytest -n number`
+
+### View beautiful reports
+1) Install allure ([instruction](https://docs.qameta.io/allure/#_installing_a_commandline))
+2) Run tests
+3) Type `allure generate report --clean && allure open allure-report`
+
